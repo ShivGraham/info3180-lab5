fe880d58caeac0b846002ee190d4158ada9f3f70
Shivaun Graham <shivaun.graham@mymona.uwi.edu>
2018-03-01 22:51:07 +0000

    added user and updated login route

--- app/views.py
+++ app/views.py
@@ -32,21 +32,26 @@ def about():
 def login():
     form = LoginForm()
     if request.method == "POST":
-        # change this to actually validate the entire form submission
+        # changed this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
 
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            u_name = form.username.data
+            pwd = form.password.data
+            
+            user = UserProfile.query.filter_by(username=u_name, password=pwd).first()
+            
             # get user id, load into session
             login_user(user)
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            flash('Successfully logged in', 'success')
+            return redirect(url_for('secure-page'))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
